650b32c6ee419cdd76d4a1db6a14a88153242cc9
Erik Ecoologic <erikecoologic@gmail.com>
2021-07-06 22:07:48 +1000

    Form

--- src/Content/index.tsx
+++ src/Content/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import { Button, TextField } from '@material-ui/core';
+import React, { ChangeEvent, useState } from 'react';
 import Graph from '../Graph';
+
+const Form = () => {
+  const [values, setValues] = useState<Record<string, string>>({});
+  const onChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    setValues((values) => ({ ...values, [ev.target.id]: ev.target.value }));
+  };
+  return (
+    <form>
+      <TextField
+        id="query"
+        label="Standard"
+        required
+        autoFocus
+        onChange={onChange}
+      />
+      <Button title="Push me">Submit</Button>
+      {values.query}
+    </form>
+  );
+};
 
 const Content = (): JSX.Element => {
   return (
     <main className="App__content">
+      <Form />
       <Graph />
     </main>
   );
