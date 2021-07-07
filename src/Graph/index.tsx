f07f46e4e645e5b6cd29fbfea8da7776f463c564
Erik Ecoologic <erikecoologic@gmail.com>
2021-07-07 22:04:29 +1000

    D3 <circle>

--- src/Graph/index.tsx
+++ src/Graph/index.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import * as d3 from 'd3';
 
-const graphId = 'theGraph';
-
 const Graph = (): JSX.Element => {
   const graphRef = React.useRef(null);
   React.useEffect(() => {
     // d3.select(`#${graphId}`).style("color", "green");
-    d3.select(`#${graphId}`)
-      .selectAll('div')
+    d3.select(`#nodeGroup`)
+      .selectAll('circle')
       .data([1, 2, 3])
       .enter()
-      .append('div')
-      .text((data) => data);
+      .append('circle')
+      .attr('cx', (n) => n * 10)
+      .attr('cy', (n) => n * 10)
+      .attr('r', 10);
   }, []);
   return (
-    <div id={graphId} ref={graphRef} className="Graph">
-      D3 here
+    <div id="graphId" ref={graphRef} className="Graph">
+      <svg xmlns="http://www.w3.org/2000/svg">
+        <g id="nodeGroup" fill="white" stroke="green" strokeWidth="5"></g>
+      </svg>
     </div>
   );
 };
