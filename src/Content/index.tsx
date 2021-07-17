abc9aa4e1880696dbb8ae739c7f5877fd4c53a98
Erik Ecoologic <erikecoologic@gmail.com>
2021-07-17 10:40:11 +1000

    GraphForm/index

--- src/Content/index.tsx
+++ src/Content/index.tsx
@@ -1,83 +1,8 @@
-import { ButtonGroup, Grid, TextField } from '@material-ui/core';
-import React, { ChangeEvent, FC, useState } from 'react';
+import { Grid } from '@material-ui/core';
+import { FC } from 'react';
 import Graph from '../Graph';
-import { IHasOnSubmit, IonSubmit } from '../interfaces';
-import Button from '../utils/Button';
-import useStyles from './useStyles';
-
-type IGraphFormFields = 'name' | 'size';
-interface IGraphFormValues {
-  name: string;
-  size: number;
-}
-
-const GraphForm: FC<IHasOnSubmit> = ({ onSubmit }) => {
-  const classes = useStyles();
-  const blankValues = { name: '', size: 0 };
-  const [values, setValues] = useState<IGraphFormValues>(blankValues);
-
-  const onChange = (ev: ChangeEvent<HTMLInputElement>) => {
-    console.log(`old values`, values);
-    setValues((values) => ({ ...values, [ev.target.id]: ev.target.value }));
-  };
-
-  const valid = (field: IGraphFormFields): boolean => {
-    const value = values[field];
-    const fieldValidator = {
-      name: () => !!value,
-      size: () => value > 0,
-    }[field];
-    console.log(`valid ${field} `, fieldValidator, fieldValidator());
-    return fieldValidator();
-  };
-
-  const isEnabled = (): boolean => true;
-
-  // TODO: can we use onSubmit in the form?
-  return (
-    <form>
-      <Grid container spacing={3}>
-        <Grid item md={10}>
-          <TextField
-            id="name"
-            label="Name"
-            type="text"
-            required
-            autoFocus
-            fullWidth={true}
-            onChange={onChange}
-            error={!valid('name')}
-            helperText={valid('name') ? 'Must be present' : ''}
-          />
-        </Grid>
-        <Grid item md={2}>
-          <TextField
-            id="size"
-            label="Size"
-            type="number"
-            required
-            onChange={onChange}
-            fullWidth={true}
-            error={!valid('size')}
-            helperText={valid('size') ? 'A positive value' : ''}
-          />
-        </Grid>
-        <Grid item md={12}>
-          <ButtonGroup fullWidth={true}>
-            <Button
-              enabled={isEnabled()}
-              title="Push me"
-              onClick={() => onSubmit<IGraphFormValues>(values)}
-              fullWidth={true}
-            >
-              Submit
-            </Button>
-          </ButtonGroup>
-        </Grid>
-      </Grid>
-    </form>
-  );
-};
+import GraphForm from '../GraphForm';
+import { IonSubmit } from '../interfaces';
 
 const Content: FC = () => {
   const onSubmit: IonSubmit = (values) => {
---
+++ src/GraphForm/index.tsx
@@ -0,0 +1,81 @@
+import { TextField, ButtonGroup, Grid } from '@material-ui/core';
+import Button from '../utils/Button';
+import { ChangeEvent, FC, useState } from 'react';
+import useStyles from '../Content/useStyles';
+import { IHasOnSubmit } from '../interfaces';
+
+type IGraphFormFields = 'name' | 'size';
+interface IGraphFormValues {
+  name: string;
+  size: number;
+}
+
+const GraphForm: FC<IHasOnSubmit> = ({ onSubmit }) => {
+  const classes = useStyles();
+  const blankValues = { name: '', size: 0 };
+  const [values, setValues] = useState<IGraphFormValues>(blankValues);
+
+  const onChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    console.log(`old values`, values);
+    setValues((values) => ({ ...values, [ev.target.id]: ev.target.value }));
+  };
+
+  const valid = (field: IGraphFormFields): boolean => {
+    const value = values[field];
+    const fieldValidator = {
+      name: () => !!value,
+      size: () => value > 0,
+    }[field];
+    console.log(`valid ${field} `, fieldValidator, fieldValidator());
+    return fieldValidator();
+  };
+
+  const isEnabled = (): boolean => true;
+
+  // TODO: can we use onSubmit in the form?
+  return (
+    <form>
+      <Grid container spacing={3}>
+        <Grid item md={10}>
+          <TextField
+            id="name"
+            label="Name"
+            type="text"
+            required
+            autoFocus
+            fullWidth={true}
+            onChange={onChange}
+            error={!valid('name')}
+            helperText={valid('name') ? 'Must be present' : ''}
+          />
+        </Grid>
+        <Grid item md={2}>
+          <TextField
+            id="size"
+            label="Size"
+            type="number"
+            required
+            onChange={onChange}
+            fullWidth={true}
+            error={!valid('size')}
+            helperText={valid('size') ? 'A positive value' : ''}
+          />
+        </Grid>
+        <Grid item md={12}>
+          <ButtonGroup fullWidth={true}>
+            <Button
+              enabled={isEnabled()}
+              title="Push me"
+              onClick={() => onSubmit<IGraphFormValues>(values)}
+              fullWidth={true}
+            >
+              Submit
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
+
+export default GraphForm;
