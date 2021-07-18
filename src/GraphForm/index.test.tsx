9a8f5e00dc65954dae67adc30b54322a0a5c0c7a
Erik Ecoologic <erikecoologic@gmail.com>
2021-07-18 18:07:22 +1000

    GraphForm/index.test

--- src/GraphForm/index.test.tsx
+++ src/GraphForm/index.test.tsx
@@ -1,14 +1,26 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import GraphForm from '.';
 
 describe('GraphForm', () => {
   it(`updates the caller after submission`, async () => {
     const callback = jest.fn();
 
-    const { findByRole } = render(<GraphForm onSubmit={callback} />);
+    const { getByRole, container } = render(<GraphForm onSubmit={callback} />);
 
-    expect(await findByRole('textbox', { name: /name/i })).toBeInstanceOf(
-      HTMLInputElement
-    );
+    const nameInput = getByRole('textbox', { name: /Name/i });
+    const sizeInput = getByRole('spinbutton', { name: /Size/i });
+    // const submit = getByRole('button', { name: /Submit/i }); // Doesn't work
+    // const submit = getByText(/Submit/i); // Also Works
+    const submit = container.querySelector('form button');
+
+    fireEvent.change(nameInput, { target: { value: 'test' } });
+    fireEvent.change(sizeInput, { target: { value: '1' } });
+    if (submit) {
+      fireEvent.click(submit);
+    } else {
+      throw 'no submit button';
+    }
+
+    expect(callback.mock.calls).toEqual([[{ name: 'test', size: 1 }]]);
   });
 });
